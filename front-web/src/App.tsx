6526c25fea9eec4830bc1568d24dbb31ea825aee
Maria Silvania <silvania.simoes@hotmail.com>
2021-01-09 00:41:04 -0300

    Finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,14 @@
-import './App.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Routes from './Routes';
+import './App.css';
 
 function App() {
   return (
-    <Routes/>
+    <>
+      <Routes/>
+      <ToastContainer />
+    </>
   );
 }
 
